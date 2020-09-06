d52e2ba62e5eb88ce39ae9352b7c4e9e980b8df4
alexeykomov <alexeykofficial@gmail.com>
2020-09-06 17:58:34 +0300

    feat: add details route

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/categories', usersRouter);
-app.use('/detailsRouter/:id', detailsRouter);
+app.use('/details/:id', detailsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
