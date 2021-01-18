dcf0695e96ca71d5ab0469da59e29f39f20114b3
alexeykomov <alexeykofficial@gmail.com>
2021-01-18 12:00:15 +0300

    add: linked categories

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/categories');
+var categoriesRouter = require('./routes/categories');
 var detailsRouter = require('./routes/details');
 
 var app = express();
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/categories', usersRouter);
+app.use('/categories', categoriesRouter);
 app.use('/details/:id', detailsRouter);
 
 // catch 404 and forward to error handler
--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const categories = require('../public/javascripts/categories-payload.json');
+const categories = require('../public/javascripts/object.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
